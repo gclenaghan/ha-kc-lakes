a31060ea26bac6a0b9a64f17206a7c2da126664d
Graham Clenaghan <gclenaghan@gmail.com>
2025-05-02 09:09:44 -0700

    Fix missing timezone

--- custom_components/kc_lakes/coordinator.py
+++ custom_components/kc_lakes/coordinator.py
@@ -1,8 +1,11 @@
 """DataUpdateCoordinator for King County Lakes."""
+
 import logging
 import async_timeout
 from dateutil.parser import parse
+from datetime import tzinfo
 from typing import Dict, Any
+from zoneinfo import ZoneInfo
 
 from aiohttp import ClientSession, ClientError, ClientResponseError
 
@@ -12,7 +15,20 @@ from homeassistant.helpers.update_coordinator import (
 )
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, API_URL, API_TIMEOUT, SCAN_INTERVAL, LAKES
+from .const import (
+    DOMAIN,
+    API_URL,
+    API_TIMEOUT,
+    SCAN_INTERVAL,
+    KEY_WEATHER_LAST_UPDATE,
+    KEY_AIR_TEMPERATURE,
+    KEY_WIND_SPEED,
+    KEY_WIND_DIRECTION,
+    KEY_WATER_TEMPERATURE,
+    KEY_WATER_LAST_UPDATE,
+    KEY_BUOY_LATITUDE,
+    KEY_BUOY_LONGITUDE,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,39 +50,48 @@ class KCLakesDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Any]]):
         try:
             async with async_timeout.timeout(API_TIMEOUT):
                 response = await self.session.get(API_URL)
-                response.raise_for_status() # Raise exception for bad status codes (4xx or 5xx)
+                response.raise_for_status()  # Raise exception for bad status codes (4xx or 5xx)
                 # Assuming the API returns a list of dictionaries, one for each lake/buoy
                 raw_data = await response.text()
 
                 return self.parse_buoy_data(raw_data)
 
         except ClientResponseError as err:
-             _LOGGER.error("HTTP error fetching data: %s - %s", err.status, err.message)
-             raise UpdateFailed(f"Error communicating with API: {err}") from err
+            _LOGGER.error("HTTP error fetching data: %s - %s", err.status, err.message)
+            raise UpdateFailed(f"Error communicating with API: {err}") from err
         except ClientError as err:
-             _LOGGER.error("Client error fetching data: %s", err)
-             raise UpdateFailed(f"Error communicating with API: {err}") from err
+            _LOGGER.error("Client error fetching data: %s", err)
+            raise UpdateFailed(f"Error communicating with API: {err}") from err
         except TimeoutError:
-             _LOGGER.error("Timeout fetching data")
-             raise UpdateFailed("Timeout communicating with API") from TimeoutError
+            _LOGGER.error("Timeout fetching data")
+            raise UpdateFailed("Timeout communicating with API") from TimeoutError
         except Exception as err:
-             _LOGGER.exception("Unexpected error fetching data") # Logs the full traceback
-             raise UpdateFailed(f"Unexpected error: {err}") from err
-        
+            _LOGGER.exception(
+                "Unexpected error fetching data"
+            )  # Logs the full traceback
+            raise UpdateFailed(f"Unexpected error: {err}") from err
+
     def parse_buoy_data(self, raw_data):
         processed_data = {}
-        for lake_raw_data in raw_data.split('^|'):
-            lake_str_columns = lake_raw_data.split('|\t')
-            if len(lake_str_columns) < 10 or lake_str_columns[9] != 'Y':
+        for lake_raw_data in raw_data.split("^|"):
+            lake_str_columns = lake_raw_data.split("|\t")
+            if len(lake_str_columns) < 10 or lake_str_columns[9] != "Y":
                 continue
-            lake_raw_data[lake_str_columns[0]] = {
-                'weather_last_update': parse(lake_str_columns[1]),
-                'air_temperature_c': float(lake_str_columns[2]),
-                'wind_speed_m/s': float(lake_str_columns[3]),
-                'wind_direction': lake_str_columns[4][5:], #[5:] removes the "from " prefix
-                'water_temperature': float(lake_str_columns[5]),
-                'water_last_update': parse(lake_str_columns[6]),
-                'buoy_latitude': float(lake_str_columns[7]),
-                'buoy_longitude': float(lake_str_columns[8]),
+            pacific_zone = ZoneInfo("America/Los_Angeles")
+            processed_data[lake_str_columns[0]] = {
+                KEY_WEATHER_LAST_UPDATE: parse(lake_str_columns[1]).replace(
+                    tzinfo=pacific_zone
+                ),
+                KEY_AIR_TEMPERATURE: float(lake_str_columns[2]),
+                KEY_WIND_SPEED: float(lake_str_columns[3]),
+                KEY_WIND_DIRECTION: lake_str_columns[4][
+                    5:
+                ],  # [5:] removes the "from " prefix
+                KEY_WATER_TEMPERATURE: float(lake_str_columns[5]),
+                KEY_WATER_LAST_UPDATE: parse(lake_str_columns[6]).replace(
+                    tzinfo=pacific_zone
+                ),
+                KEY_BUOY_LATITUDE: float(lake_str_columns[7]),
+                KEY_BUOY_LONGITUDE: float(lake_str_columns[8]),
             }
         return processed_data
